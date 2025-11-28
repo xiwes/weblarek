b95de5eed3cf4f5f369dce5a82a2844a3cf9bc59
xiwes <152086132+xiwes@users.noreply.github.com>
2025-11-28 19:54:43 +0300

    Update LarekApi.ts

--- src/components/LarekApi.ts
+++ src/components/LarekApi.ts
@@ -1,25 +1,25 @@
-import { IApi, IProduct, IOrder } from '../types';
-
-/**
- * Класс коммуникационного слоя.
- * Отвечает за получение товаров с сервера и отправку заказов.
- */
-export class LarekApi {
-  private api: IApi;
-
-  constructor(api: IApi) {
-    this.api = api;
-  }
-
-  // GET /product/
-  async getProducts(): Promise<IProduct[]> {
-    const response = await this.api.get<{ items: IProduct[] }>('/product/');
-    return response.items;
-  }
-
-  // POST /order/
-  async sendOrder(order: IOrder): Promise<unknown> {
-    const response = await this.api.post('/order/', order);
-    return response;
-  }
+import { IApi, IProduct, IOrder } from '../types'; 
+ 
+/** 
+ * Класс коммуникационного слоя. 
+ * Отвечает за получение товаров с сервера и отправку заказов. 
+ */ 
+export class LarekApi { 
+  private api: IApi; 
+ 
+  constructor(api: IApi) { 
+    this.api = api; 
+  } 
+ 
+  // GET /product/ 
+  async getProducts(): Promise<IProduct[]> { 
+    const response = await this.api.get<{ items: IProduct[] }>('/product/'); 
+    return response.items; 
+  } 
+ 
+  // POST /order/ 
+  async sendOrder(order: IOrder): Promise<{ id: string; total: number }> { 
+    const response = await this.api.post<{ id: string; total: number }>('/order/', order); 
+    return response; 
+  } 
 }
